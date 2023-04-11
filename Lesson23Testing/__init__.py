4f852c9346279ca54608aa91559eaf19fa8d72ba
Hourgund <nikitacepikov7@gmail.com>
2023-04-11 20:16:06 +0300

    Project 23, added defolt-constructor

--- Lesson23Testing/__init__.py
+++ Lesson23Testing/__init__.py
@@ -1,21 +1,41 @@
 from student import Student
+from student import init
 
 
 def main():
     st1 = Student()
     st2 = Student()
-
-    st1.name = "Alex"
-    st2.name = "Anna"
-    st1.mark = 10
-    st2.mark = 7
-    st1.age = 20
-    st2.age = 18
-
-    print(st1.name)
-    print(getattr(st1, "name"))
-
     print(vars(st1))
+    print(vars(st2))
+
+#    init(st1, "Alex", 20, 10)
+#    init(st1, "Kate", 18, 9)
+
+#    st1.name = "Alex"
+#    st2.name = "Anna"
+#    st1.mark = 10
+#    st2.mark = 7
+#    st1.age = 20
+#    st2.age = 18
+
+#    print(st1.name)
+#    print(getattr(st1, "name"))
+#    print(st1.__getattribute__("name"))
+
+#    del st1.name
+#    delattr(st1, "mark")
+#    st1.__delattr__("age")
+
+#    st1.name = "Alex"
+#   setattr(st1, "name", "Peter")
+#    st1.__setattr__("name", "Olya")
+
+#   print(vars(st1))
+
+
+#    print(st1.__dict__)
+#    print(dir(st1))
+#    print(help(st1))
 
 
 if __name__ == '__main__':
--- Lesson23Testing/student.py
+++ Lesson23Testing/student.py
@@ -1,2 +1,15 @@
 class Student:
-    pass
+
+    def __init__(self):
+        self.name = "no name"
+        self.age = 16
+        self.mark = 4
+
+
+#      print("Calling defoult-costructor")
+
+
+def init(st, name, age, mark):
+    st.name = name
+    st, age = age
+    st.mark = mark
