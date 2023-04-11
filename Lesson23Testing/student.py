66e786d2f1524e199705e2a6983f88b195c3abf3
Hourgund <nikitacepikov7@gmail.com>
2023-04-11 20:29:58 +0300

    Project 23, added constructor with arguments

--- Lesson23Testing/student.py
+++ Lesson23Testing/student.py
@@ -1,15 +1,16 @@
 class Student:
 
-    def __init__(self):
-        self.name = "no name"
-        self.age = 16
-        self.mark = 4
+    def __init__(self, name, age, mark):
+        print("Calling costructor with arguments")
+        self.name = name
+        self.age = age
+        self.mark = mark
 
 
-#      print("Calling defoult-costructor")
+#      print("Calling defolt-costructor")
 
 
 def init(st, name, age, mark):
     st.name = name
-    st, age = age
+    st.age = age
     st.mark = mark
