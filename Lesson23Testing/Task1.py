b9252dbba43081aa59eafd0ad894e7b382438a08
Hourgund <nikitacepikov7@gmail.com>
2023-04-11 21:37:31 +0300

    Added script of program

--- Lesson23Testing/Task1.py
+++ Lesson23Testing/Task1.py
@@ -1,11 +1,12 @@
 from student import Student
+from manager import Manager
 
 
-def main():                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
-    st1 = Student("Alex", 20, 10)
-    st2 = Student("Kate", 18, 7)
-    st3 = Student("Peter", 21, 9)
-    st4 = Student()
+# def main():
+#    st1 = Student("Alex", 20, 10)
+#    st2 = Student("Kate", 18, 7)
+#    st3 = Student("Peter", 21, 9)
+#    st4 = Student()
 
 
 #    init(st1, "Alex", 20, 10)
@@ -36,6 +37,17 @@ def main():
 #    print(st1.__dict__)
 #    print(dir(st1))
 #    print(help(st1))
+def main():
+    manager = Manager()
+    s1 = Student("Alex", 20, 10)
+    s2 = Student("Kate", 18, 9)
+    s3 = Student("Peter", 22, 5)
+
+    ls = [s1, s2, "abc", s3, 5]
+
+    result = manager.calc_avg_mark(ls)
+
+    print(f"Result is {result}.")
 
 
 if __name__ == '__main__':
